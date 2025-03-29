09d6794ce42afdc6f6d03f25d0bdbdd6942477a9
Ziyad Sfaxi <ziyadsfaxi@Ziyads-MacBook-Pro-2.local>
2025-03-29 11:40:56 +1100

    Update nvim & tmux

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,67 +6,6 @@ return {
       tailwindcss = {
         filetypes_exclude = { "css", "sass", "php" },
       },
-
-      intelephense = {
-        enabled = false,
-        commands = {
-          IntelephenseIndex = {
-            function()
-              vim.lsp.buf.execute_command({ command = "intelephense.index.workspace" })
-            end,
-          },
-        },
-      },
-
-      volar = {
-        init_options = {
-          vue = {
-            hybridMode = true,
-          },
-        },
-        -- on_attach = function(client, bufnr)
-        --   client.server_capabilities.documentFormattingProvider = false
-        --   client.server_capabilities.documentRangeFormattingProvider = false
-        -- end,
-      },
-
-      vtsls = {
-        enabled = false,
-      },
-
-      ts_ls = {
-        init_options = {
-          plugins = {
-            {
-              name = "@vue/typescript-plugin",
-              location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
-              languages = { "vue" },
-            },
-          },
-        },
-
-        settings = {
-          typescript = {
-
-            inlayHints = {
-              includeInlayParameterNameHints = "all",
-              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-              includeInlayFunctionParameterTypeHints = true,
-              includeInlayVariableTypeHints = true,
-              includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-              includeInlayPropertyDeclarationTypeHints = true,
-              includeInlayFunctionLikeReturnTypeHints = true,
-              includeInlayEnumMemberValueHints = true,
-            },
-          },
-        },
-
-        filetypes = {
-          "javascript",
-          "typescript",
-          "vue",
-        },
-      },
     },
   },
 }
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -4,6 +4,7 @@ return {
     ensure_installed = {
       "phpactor",
       "cspell",
+      "php-debug-adapter",
     },
   },
 }
