f060185dda02a11a00da35890b1fa8e23a14a25b
ziyadsfaxi <ziyadsfaxi@gmail.com>
2025-04-01 11:44:42 +1100

    Fix mason

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -6,11 +6,11 @@ return {
       "cspell",
       "csharpier",
       "netcoredbg",
+      "php-debug-adapter",
     },
     registries = {
       "github:mason-org/mason-registry",
       "github:crashdummyy/mason-registry",
-      "php-debug-adapter",
     },
   },
 }
